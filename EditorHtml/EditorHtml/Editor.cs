607479b418cbc0d2947ead19ba947986c465214c
mathdantas <matheus.dantas.ti@gmail.com>
2021-02-04 16:48:37 -0300

    Creating a simple HTML editor

---
+++ EditorHtml/EditorHtml/Editor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace EditorHtml
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO EDITOR\n");
+            Start();
+        }
+        public static void Start()
+        {
+            var file = new StringBuilder();
+
+            do
+            {
+                file.Append(Console.ReadLine());
+                file.Append(Environment.NewLine);
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine("_Deseja salvar o arquivo?" );
+            Viewer.Show(file.ToString());
+        }
+    }
+}
